1b334361186f3563344f0262490b672b78f8c7da
mckinleyks <mckinleysaunders1210@gmail.com>
2025-01-09 12:51:13 -0700

    List of numbers prep finished

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        while (number !=0);
+
+        int sum = 0;
+        foreach (int numb in numbers)
+        {
+            sum += numb;
+        }
+
+        double average = (double)sum / numbers.Count;
+        int largest = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
+
 }
